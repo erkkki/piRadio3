e1fc7572f008385100f8d28c54b8d46ae0ddb8e4
Joonas <joonas.erkheikki@gmail.com>
2021-01-16 00:50:51 +0200

    nav auto close.

--- src/app/countries/countries/countries.component.ts
+++ src/app/countries/countries/countries.component.ts
@@ -1,21 +1,40 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CountriesService} from '../../core/services/countries.service';
 
 import { Country } from '../../core/models/country';
-import {BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html',
   styleUrls: ['./countries.component.scss']
 })
-export class CountriesComponent implements OnInit {
+export class CountriesComponent implements OnInit, OnDestroy {
 
-  countries$: BehaviorSubject<Country[]>;
+  countries: Country[];
+  filteredCountries: Country[];
+  subscription: Subscription;
+  value = '';
 
   constructor(private countryService: CountriesService) { }
 
   ngOnInit(): void {
-    this.countries$ = this.countryService.countries;
+    this.subscription = this.countryService.countries.subscribe(value => {
+      this.countries = value;
+      this.filteredCountries = value;
+    });
+  }
+
+  search(): void {
+    if (!this.countries) {
+      return;
+    }
+    this.filteredCountries = this.countries.filter((country) => {
+      return country.name.toLowerCase().startsWith(this.value.toLowerCase());
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
