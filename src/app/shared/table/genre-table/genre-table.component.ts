32e4ca085bdee6a769ba35671c7064cb371949ce
Joonas <joonas.erkheikki@gmail.com>
2021-01-20 02:44:05 +0200

    Shared
    Tables & lists

---
+++ src/app/shared/table/genre-table/genre-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-genre-table',
+  templateUrl: './genre-table.component.html',
+  styleUrls: ['./genre-table.component.scss']
+})
+export class GenreTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
